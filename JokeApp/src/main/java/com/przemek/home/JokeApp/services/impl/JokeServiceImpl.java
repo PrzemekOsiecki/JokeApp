3d56b2696642ad6f09368bc69d66da2acdc5f732
Przemek Osiecki <przemyslaw.osiecki@gmail.com>
2018-11-10 22:03:00 +0100

    Java Based Config & Constructor Injection via Lombok project

--- JokeApp/src/main/java/com/przemek/home/JokeApp/services/impl/JokeServiceImpl.java
+++ JokeApp/src/main/java/com/przemek/home/JokeApp/services/impl/JokeServiceImpl.java
@@ -2,21 +2,17 @@ package com.przemek.home.JokeApp.services.impl;
 
 import com.przemek.home.JokeApp.services.JokeService;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
-    }
-
     @Override
-
     public String getRandomJoke() {
         return chuckNorrisQuotes.getRandomQuote();
     }
